c5eeb2f541bd37f8addceff7116de7119397858a
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-08-14 21:38:35 -0300

    Update products.controller.ts

--- Nest js/src/products/products.controller.ts
+++ Nest js/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
@@ -12,6 +12,11 @@ export class ProductsController {
     return this.productsService.create(createProductDto);
   }
 
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
@@ -21,7 +26,13 @@ export class ProductsController {
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.delete(id);
+  }
 }
+
 
 
 
