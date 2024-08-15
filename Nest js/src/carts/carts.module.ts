96c6da72616c82eaf7f2d41e20333a045ae8071f
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-08-14 21:41:18 -0300

    Update carts.module.ts

--- Nest js/src/carts/carts.module.ts
+++ Nest js/src/carts/carts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CartsController } from './carts.controller';
 import { CartsService } from './carts.service';
+import { CartSchema } from './schemas/cart.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Cart', schema: CartSchema }])],
   controllers: [CartsController],
   providers: [CartsService],
 })
