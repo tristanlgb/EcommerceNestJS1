aeb608773c121d4e8c2aeb6671fca94471a63c62
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-08-14 21:46:40 -0300

    Update create-product.dto.ts

--- Nest js/src/products/dtos/create-product.dto.ts
+++ Nest js/src/products/dtos/create-product.dto.ts
@@ -1,3 +1,25 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty()
+  readonly price: number;
+
+  @IsOptional()
+  @IsString()
+  readonly description?: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty()
+  readonly quantity: number;
+}
 
 
 
