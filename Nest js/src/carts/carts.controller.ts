bd264528d2e7c3154e8d4807f25b0c4d4f4fbdc4
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-08-14 21:43:33 -0300

    Update carts.controller.ts

--- Nest js/src/carts/carts.controller.ts
+++ Nest js/src/carts/carts.controller.ts
@@ -1,19 +1,22 @@
-import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param, Req } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { AddProductDto } from './dtos/add-product.dto';
 import { UpdateQuantityDto } from './dtos/update-quantity.dto';
+import { Request } from 'express';
 
 @Controller('carts')
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
 
   @Post('add')
-  addProduct(@Body() addProductDto: AddProductDto) {
-    return this.cartsService.addProduct(addProductDto);
+  addProduct(@Body() addProductDto: AddProductDto, @Req() req: Request) {
+    const userId = req.user.id; // Assuming user ID is available in request
+    return this.cartsService.addProduct(addProductDto, userId);
   }
 
   @Put('update-quantity')
-  updateQuantity(@Body() updateQuantityDto: UpdateQuantityDto) {
-    return this.cartsService.updateQuantity(updateQuantityDto);
+  updateQuantity(@Body() updateQuantityDto: UpdateQuantityDto, @Req() req: Request) {
+    const userId = req.user.id; // Assuming user ID is available in request
+    return this.cartsService.updateQuantity(updateQuantityDto, userId);
   }
 }
