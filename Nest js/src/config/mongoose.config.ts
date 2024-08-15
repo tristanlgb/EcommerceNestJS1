cc029415a77f1ab3ede619559ac307349fb7e407
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-08-14 21:37:19 -0300

    Update mongoose.config.ts

--- Nest js/src/config/mongoose.config.ts
+++ Nest js/src/config/mongoose.config.ts
@@ -1,8 +1,12 @@
 import { MongooseModule } from '@nestjs/mongoose';
 
-export const MongooseConfig = MongooseModule.forRoot(process.env.MONGO_URI);
-
-
+export const MongooseConfig = MongooseModule.forRoot(
+  'mongodb+srv://tristanlgb:<hola123>@pokemons.gobj7y1.mongodb.net/Pokemons?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 
 
